ec85311c7e3f93fde38976107273f0de11aee2b8
HamadZafarullah <hamadzafarullah@gmail.com>
2022-10-20 17:00:48 -0400

    not done

---
+++ script.js
@@ -0,0 +1,13 @@
+// need to make a box = 9 divs
+const playerX = 'x'
+const playerO = 'circle'
+const wincombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+]
